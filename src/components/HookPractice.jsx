c54d7251606f85455ce4754b7debfab870fca29f
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-07-25 02:58:15 +0100

    We could succefully fetch data without getting an infinite loop

--- src/components/HookPractice.jsx
+++ src/components/HookPractice.jsx
@@ -6,12 +6,18 @@ export default function HookPractice(){
 
 
     useEffect(() =>{
-        console.log("Effect Run");
-    }, [count]);
+        // console.log("Effect Run");
+        fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+            .then(res => res.json())
+            .then(data => setData(data));
+    }, []);
 
     return(
         <div>
             <pre>{JSON.stringify(data, null, 3)}</pre>
+            <div>
+                <h1>{}</h1>
+            </div>
             <h1>You Clicked {count} times!</h1>
             <button onClick ={() => {setCount(count+1)}}>Add</button>
             <button onClick = {() => {setCount(0)}}>Reset</button>
